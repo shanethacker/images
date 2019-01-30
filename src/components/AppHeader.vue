90df885b51a905362f6a574740582fb3de4642d2
Shane Thacker <sst@clockworkmind.com>
2019-01-30 16:46:05 -0500

    Started app and added Semantic UI

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a class="item active">
+            Image Storage
+        </a>
+        <div class="right menu">
+            <a class="ui item">
+                Log In
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+    }
+</script>
+
+<style scoped>
+
+</style>
